3dc33c02d639534e0da7f4c76ee5293b6174005c
Vo Quoc Thanh <vqthanh1999@gmail.com>
2026-06-02 21:45:17 +0700

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 const express = require('express');
-const http = require('http');
+const http = require('https');
 const { Server } = require("socket.io");
 
 const port = 5000;
@@ -22,7 +22,7 @@ const SECRET_KEY = "backpacking_chat_secret";
 // --- MIDDLEWARE ---
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://localhost:5173" }));
 
 // --- CẤU HÌNH STATIC FILES (SỬA LẠI TẠI ĐÂY) ---
 // Đảm bảo rằng đường dẫn này trỏ thẳng đến thư mục chứa các folder 'chư nâm', 'núi bằng am'
@@ -42,7 +42,7 @@ route(app);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://localhost:5173",
     methods: ["GET", "POST"]
   }
 });
@@ -128,6 +128,6 @@ io.on("connection", (socket) => {
 // --- KẾT NỐI DB VÀ CHẠY SERVER ---
 db.connectDB().then(() => {
   server.listen(port, () => {
-    console.log(`✅ Server đang chạy tại: http://localhost:${port}`);
+    console.log(`✅ Server đang chạy tại: https://localhost:${port}`);
   });
 });
